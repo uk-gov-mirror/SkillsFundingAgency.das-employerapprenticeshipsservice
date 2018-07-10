e5187cecb4ef52f012142e72714847e25e760c08
Iain Stevenson <IainStevenson@live.co.uk>
2018-07-10 13:23:54 +0100

    Used openSSL certificate with true Subject Alternative Name

--- src/DevInstall.ps1
+++ src/DevInstall.ps1
@@ -3,7 +3,7 @@
 $certpwd = ConvertTo-SecureString -String password -Force -AsPlainText
 
 Import-PfxCertificate -FilePath $PSScriptRoot\localhost.pfx -CertStoreLocation cert://LocalMachine/My -Password $certpwd -Exportable
-Import-PfxCertificate -FilePath $PSScriptRoot\SFA.Dev.Local.pfx -CertStoreLocation cert://LocalMachine/Root -Password $certpwd -Exportable
+Import-PfxCertificate -FilePath $PSScriptRoot\localhost.pfx -CertStoreLocation cert://LocalMachine/Root -Password $certpwd -Exportable
 
 Import-PfxCertificate -FilePath $PSScriptRoot\DasAmlCert.pfx -CertStoreLocation cert://LocalMachine/My -Password $certpwd -Exportable
 Import-PfxCertificate -FilePath $PSScriptRoot\DasAmlCert.pfx -CertStoreLocation cert://LocalMachine/Root -Password $certpwd -Exportable
