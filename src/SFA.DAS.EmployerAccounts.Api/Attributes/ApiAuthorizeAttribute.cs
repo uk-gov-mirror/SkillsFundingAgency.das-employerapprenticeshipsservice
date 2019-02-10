612192b7abee795f9ece61e268f39e9b17d0af71
Steve O'Hara <github@soharadesign.com>
2019-02-10 22:22:29 +0000

    [AML-3330] Moved missing AuthorizeAttribute to SFA.DAS.EmployerAccounts.Api

---
+++ src/SFA.DAS.EmployerAccounts.Api/Attributes/ApiAuthorizeAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace SFA.DAS.EmployerAccounts.Api.Attributes
+{
+    using System;
+    using System.Web.Http;
+    using System.Web.Http.Controllers;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            return actionContext.Request.RequestUri.IsLoopback || base.IsAuthorized(actionContext);
+        }
+    }
+}
