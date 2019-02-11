45d234d9dd95613d902e3d186cc2d64312b657ce
Steve O'Hara <github@soharadesign.com>
2019-02-11 11:39:24 +0000

    [AML-3330] Added missing Orchestrators to SFA.DAS.EmployerAccounts.Api

---
+++ src/SFA.DAS.EmployerAccounts.Api/Orchestrators/AccountsOrchestrator.cs
@@ -0,0 +1,253 @@
+﻿namespace SFA.DAS.EmployerAccounts.Api.Orchestrators
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+
+    using MediatR;
+
+    using SFA.DAS.HashingService;
+    using SFA.DAS.NLog.Logger;
+
+    public class AccountsOrchestrator
+    {
+        private readonly IMediator _mediator;
+        private readonly ILog _logger;
+        private readonly IMapper _mapper;
+        private readonly IHashingService _hashingService;
+
+        public AccountsOrchestrator(IMediator mediator, ILog logger, IMapper mapper, IHashingService hashingService)
+        {
+            if (mediator == null)
+                throw new ArgumentNullException(nameof(mediator));
+            this._mediator = mediator;
+            this._logger = logger;
+            this._mapper = mapper;
+            this._hashingService = hashingService;
+        }
+
+        public async Task<OrchestratorResponse<PagedApiResponseViewModel<AccountWithBalanceViewModel>>> GetAllAccountsWithBalances(string toDate, int pageSize, int pageNumber)
+        {
+            this._logger.Info("Getting all account balances.");
+
+            toDate = toDate ?? DateTime.MaxValue.ToString("yyyyMMddHHmmss");
+
+            var accountsResult = await this._mediator.SendAsync(new GetPagedEmployerAccountsQuery { ToDate = toDate, PageSize = pageSize, PageNumber = pageNumber });
+            var transactionResult = await this._mediator.SendAsync(new GetAccountBalancesRequest
+            {
+                AccountIds = accountsResult.Accounts.Select(account => account.Id).ToList()
+            });
+
+            var data = new List<AccountWithBalanceViewModel>();
+
+            var accountBalanceHash = this.BuildAccountBalanceHash(transactionResult.Accounts);
+
+            accountsResult.Accounts.ForEach(account =>
+            {
+                var accountBalanceModel = new AccountWithBalanceViewModel
+                {
+                    AccountId = account.Id,
+                    AccountName = account.Name,
+                    AccountHashId = account.HashedId,
+                    PublicAccountHashId = account.PublicHashedId,
+                    IsLevyPayer = true
+                };
+
+                if (accountBalanceHash.TryGetValue(account.Id, out var accountBalance))
+                {
+                    accountBalanceModel.Balance = accountBalance.Balance;
+                    accountBalanceModel.RemainingTransferAllowance = accountBalance.RemainingTransferAllowance;
+                    accountBalanceModel.StartingTransferAllowance = accountBalance.StartingTransferAllowance;
+                    accountBalanceModel.IsLevyPayer = accountBalance.IsLevyPayer == 1;
+                }
+
+                data.Add(accountBalanceModel);
+            });
+
+            return new OrchestratorResponse<PagedApiResponseViewModel<AccountWithBalanceViewModel>> { Data = new PagedApiResponseViewModel<AccountWithBalanceViewModel> { Data = data, Page = pageNumber, TotalPages = (accountsResult.AccountsCount / pageSize) + 1 } };
+        }
+
+        private Dictionary<long, AccountBalance> BuildAccountBalanceHash(List<AccountBalance> accountBalances)
+        {
+            var result = new Dictionary<long, AccountBalance>(accountBalances.Count);
+
+            foreach (var balance in accountBalances)
+            {
+                result.Add(balance.AccountId, balance);
+            }
+
+            return result;
+        }
+
+        public async Task<OrchestratorResponse<AccountDetailViewModel>> GetAccount(long accountId)
+        {
+            var hashedAccountId = this._hashingService.HashValue(accountId);
+
+            if (string.IsNullOrWhiteSpace(hashedAccountId))
+            {
+                return new OrchestratorResponse<AccountDetailViewModel> { Data = null };
+            }
+
+            var response = await this.GetAccount(hashedAccountId);
+            return response;
+        }
+
+        public async Task<OrchestratorResponse<AccountDetailViewModel>> GetAccount(string hashedAccountId)
+        {
+            this._logger.Info($"Getting account {hashedAccountId}");
+
+            var accountResult = await this._mediator.SendAsync(new GetEmployerAccountByHashedIdQuery { HashedAccountId = hashedAccountId });
+            if (accountResult.Account == null)
+            {
+                return new OrchestratorResponse<AccountDetailViewModel> { Data = null };
+            }
+
+            var viewModel = ConvertAccountDetailToViewModel(accountResult);
+
+            var accountBalanceTask = this.GetBalanceForAccount(accountResult.Account.AccountId);
+            var transferBalanceTask = this.GetTransferAllowanceForAccount(accountResult.Account.AccountId);
+
+            await Task.WhenAll(accountBalanceTask, transferBalanceTask).ConfigureAwait(false);
+
+            viewModel.Balance = accountBalanceTask.Result;
+            viewModel.RemainingTransferAllowance = transferBalanceTask.Result.RemainingTransferAllowance ?? 0;
+            viewModel.StartingTransferAllowance = transferBalanceTask.Result.StartingTransferAllowance ?? 0;
+
+            return new OrchestratorResponse<AccountDetailViewModel> { Data = viewModel };
+        }
+
+        public async Task<OrchestratorResponse<PayeSchemeViewModel>> GetPayeScheme(string hashedAccountId, string payeSchemeRef)
+        {
+            this._logger.Info($"Getting paye scheme {payeSchemeRef} for account {hashedAccountId}");
+
+            var payeSchemeResult = await this._mediator.SendAsync(new GetPayeSchemeByRefQuery { HashedAccountId = hashedAccountId, Ref = payeSchemeRef });
+            if (payeSchemeResult.PayeScheme == null)
+            {
+                return new OrchestratorResponse<PayeSchemeViewModel> { Data = null };
+            }
+
+            var viewModel = ConvertPayeSchemeToViewModel(hashedAccountId, payeSchemeResult);
+            return new OrchestratorResponse<PayeSchemeViewModel> { Data = viewModel };
+        }
+
+        public async Task<OrchestratorResponse<ICollection<TeamMemberViewModel>>> GetAccountTeamMembers(long accountId)
+        {
+            var hashedAccountId = this._hashingService.HashValue(accountId);
+
+            var response = await this.GetAccountTeamMembers(hashedAccountId);
+
+            return response;
+
+        }
+
+        public async Task<OrchestratorResponse<ICollection<TeamMemberViewModel>>> GetAccountTeamMembers(string hashedAccountId)
+        {
+            this._logger.Info($"Requesting team members for account {hashedAccountId}");
+
+            var teamMembers = await this._mediator.SendAsync(new GetTeamMembersRequest { HashedAccountId = hashedAccountId });
+
+            var memberViewModels = teamMembers.TeamMembers.Select(x => this._mapper.Map<TeamMemberViewModel>(x)).ToList();
+
+            return new OrchestratorResponse<ICollection<TeamMemberViewModel>>
+            {
+                Data = memberViewModels,
+                Status = HttpStatusCode.OK
+            };
+        }
+
+        public async Task<OrchestratorResponse<AccountResourceList<LevyDeclarationViewModel>>> GetLevy(string hashedAccountId)
+        {
+            this._logger.Info($"Requesting levy declaration for account {hashedAccountId}");
+
+            var levyDeclarations = await this._mediator.SendAsync(new GetLevyDeclarationRequest { HashedAccountId = hashedAccountId });
+            if (levyDeclarations.Declarations == null)
+            {
+                return new OrchestratorResponse<AccountResourceList<LevyDeclarationViewModel>> { Data = null };
+            }
+
+            var levyViewModels = levyDeclarations.Declarations.Select(x => this._mapper.Map<LevyDeclarationViewModel>(x)).ToList();
+            levyViewModels.ForEach(x => x.HashedAccountId = hashedAccountId);
+
+            return new OrchestratorResponse<AccountResourceList<LevyDeclarationViewModel>>
+            {
+                Data = new AccountResourceList<LevyDeclarationViewModel>(levyViewModels),
+                Status = HttpStatusCode.OK
+            };
+        }
+
+        public async Task<OrchestratorResponse<AccountResourceList<LevyDeclarationViewModel>>> GetLevy(string hashedAccountId, string payrollYear, short payrollMonth)
+        {
+            this._logger.Info($"Requesting levy declaration for account {hashedAccountId}, year {payrollYear} and month {payrollMonth}");
+
+            var levyDeclarations = await this._mediator.SendAsync(new GetLevyDeclarationsByAccountAndPeriodRequest { HashedAccountId = hashedAccountId, PayrollYear = payrollYear, PayrollMonth = payrollMonth });
+            if (levyDeclarations.Declarations == null)
+            {
+                return new OrchestratorResponse<AccountResourceList<LevyDeclarationViewModel>> { Data = null };
+            }
+
+            var levyViewModels = levyDeclarations.Declarations.Select(x => this._mapper.Map<LevyDeclarationViewModel>(x)).ToList();
+            levyViewModels.ForEach(x => x.HashedAccountId = hashedAccountId);
+
+            return new OrchestratorResponse<AccountResourceList<LevyDeclarationViewModel>>
+            {
+                Data = new AccountResourceList<LevyDeclarationViewModel>(levyViewModels),
+                Status = HttpStatusCode.OK
+            };
+        }
+
+        private PayeSchemeViewModel ConvertPayeSchemeToViewModel(string hashedAccountId, GetPayeSchemeByRefResponse payeSchemeResult)
+        {
+            var payeSchemeViewModel = new PayeSchemeViewModel
+            {
+                DasAccountId = hashedAccountId,
+                Name = payeSchemeResult.PayeScheme.Name,
+                Ref = payeSchemeResult.PayeScheme.Ref,
+                AddedDate = payeSchemeResult.PayeScheme.AddedDate,
+                RemovedDate = payeSchemeResult.PayeScheme.RemovedDate
+            };
+
+            return payeSchemeViewModel;
+        }
+
+        private static AccountDetailViewModel ConvertAccountDetailToViewModel(GetEmployerAccountByHashedIdResponse accountResult)
+        {
+            var accountDetailViewModel = new AccountDetailViewModel
+            {
+                AccountId = accountResult.Account.AccountId,
+                HashedAccountId = accountResult.Account.HashedId,
+                PublicHashedAccountId = accountResult.Account.PublicHashedId,
+                DateRegistered = accountResult.Account.CreatedDate,
+                OwnerEmail = accountResult.Account.OwnerEmail,
+                DasAccountName = accountResult.Account.Name,
+                LegalEntities = new ResourceList(accountResult.Account.LegalEntities.Select(x => new ResourceViewModel { Id = x.ToString() })),
+                PayeSchemes = new ResourceList(accountResult.Account.PayeSchemes.Select(x => new ResourceViewModel { Id = x }))
+            };
+
+            return accountDetailViewModel;
+        }
+
+        private async Task<decimal> GetBalanceForAccount(long accountId)
+        {
+            var balanceResult = await this._mediator.SendAsync(new GetAccountBalancesRequest
+            {
+                AccountIds = new List<long> { accountId }
+            });
+
+            var account = balanceResult?.Accounts?.SingleOrDefault();
+            return account?.Balance ?? 0;
+        }
+
+        private async Task<TransferAllowance> GetTransferAllowanceForAccount(long accountId)
+        {
+            var transferAllowanceResult = await this._mediator.SendAsync(new GetTransferAllowanceQuery
+            {
+                AccountId = accountId
+            });
+
+            return transferAllowanceResult.TransferAllowance;
+        }
+    }
+}
