d376bbcb5cbade0c045203f8b234b3dc235066df
Steve O'Hara <github@soharadesign.com>
2019-02-11 11:39:24 +0000

    [AML-3330] Added missing Orchestrators to SFA.DAS.EmployerAccounts.Api

---
+++ src/SFA.DAS.EmployerAccounts.Api/Orchestrators/AgreementOrchestrator.cs
@@ -0,0 +1,33 @@
+﻿namespace SFA.DAS.EmployerAccounts.Api.Orchestrators
+{
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+
+    using MediatR;
+
+    public class AgreementOrchestrator
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public AgreementOrchestrator(IMediator mediator, IMapper mapper)
+        {
+            this._mediator = mediator;
+            this._mapper = mapper;
+        }
+        
+        public async Task<OrchestratorResponse<EmployerAgreementView>> GetAgreement(string hashedAgreementId)
+        {
+            var response = await this._mediator.SendAsync(new GetEmployerAgreementByIdRequest
+            {
+                HashedAgreementId = hashedAgreementId
+            });
+
+            return new OrchestratorResponse<EmployerAgreementView>
+            {
+                Data = this._mapper.Map<EmployerAgreementView>(response.EmployerAgreement)
+            };
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerAccounts.Api/Orchestrators/UsersOrchestrator.cs
@@ -0,0 +1,45 @@
+﻿namespace SFA.DAS.EmployerAccounts.Api.Orchestrators
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+
+    using MediatR;
+
+    using SFA.DAS.NLog.Logger;
+
+    public class UsersOrchestrator
+    {
+        private readonly IMediator _mediator;
+        private readonly ILog _logger;
+        private readonly IMapper _mapper;
+
+        public UsersOrchestrator(IMediator mediator, ILog logger, IMapper mapper)
+        {
+            if (mediator == null)
+                throw new ArgumentNullException(nameof(mediator));
+
+            this._mediator = mediator;
+            this._logger = logger;
+            this._mapper = mapper;
+        }
+
+        public async Task<OrchestratorResponse<ICollection<AccountDetailViewModel>>> GetUserAccounts(string userRef)
+        {
+            this._logger.Info($"Requesting user's accounts for user Ref  {userRef}");
+
+            var accounts = await this._mediator.SendAsync(new GetUserAccountsQuery {UserRef = userRef});
+
+            var viewModels = accounts.Accounts.AccountList.Select(x => this._mapper.Map<AccountDetailViewModel>(x)).ToList();
+
+            return new OrchestratorResponse<ICollection<AccountDetailViewModel>>
+            {
+                Data = viewModels,
+                Status = HttpStatusCode.OK
+            };
+        }
+    }
+}
