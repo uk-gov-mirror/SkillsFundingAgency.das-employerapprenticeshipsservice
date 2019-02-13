96338019bbc302c95f1d44799c245025a827f34d
Steve O'Hara <github@soharadesign.com>
2019-02-13 13:44:03 +0000

    [AML-3330] Added missing Mappers to EmployerAccounts.Api

---
+++ src/SFA.DAS.EmployerAccounts.Api/Mappings/AccountMappings.cs
@@ -0,0 +1,17 @@
+﻿namespace SFA.DAS.EmployerAccounts.Api.Mappings
+{
+    using AutoMapper;
+
+    public class AccountMappings : Profile
+    {
+        public AccountMappings()
+        {
+            this.CreateMap<Domain.Models.Account.Account, AccountDetailViewModel>()
+                .ForMember(target => target.HashedAccountId, opt => opt.MapFrom(src => src.HashedId))
+                .ForMember(target => target.DasAccountName, opt => opt.MapFrom(src => src.Name));
+            this.CreateMap<LevyDeclarationView, LevyDeclarationViewModel>()
+                .ForMember(target => target.PayeSchemeReference, opt => opt.MapFrom(src => src.EmpRef));
+            this.CreateMap<Domain.Models.EmployerAgreement.EmployerAgreementView, EmployerAgreementView>();
+        }
+    }
+}
