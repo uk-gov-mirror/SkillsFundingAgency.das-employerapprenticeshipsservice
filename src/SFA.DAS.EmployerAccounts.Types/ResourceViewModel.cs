9f96022230b3f5de2bdc825d85d25c26940ed454
Steve O'Hara <github@soharadesign.com>
2019-02-13 17:46:05 +0000

    [AML-3330] Migrated missing Helpers to EmployerAccounts.Api
    [AML-3330] Migrated missing ResourceViewModel to EmployerAccounts.Types
    [AML-3330] Migrated missing ActivityMappings to EmployerAccounts.Web

---
+++ src/SFA.DAS.EmployerAccounts.Types/ResourceViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.EmployerAccounts.Types
+{
+    // TODO: relocate?
+    public class ResourceViewModel : IAccountResource
+    {
+        public string Id { get; set; }
+        public string Href { get; set; }
+    }
+}
