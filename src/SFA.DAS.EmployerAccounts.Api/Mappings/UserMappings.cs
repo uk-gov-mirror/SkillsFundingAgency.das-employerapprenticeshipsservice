7a8102958889193e55080a29c499c7d40761676f
Steve O'Hara <github@soharadesign.com>
2019-02-13 13:44:03 +0000

    [AML-3330] Added missing Mappers to EmployerAccounts.Api

---
+++ src/SFA.DAS.EmployerAccounts.Api/Mappings/UserMappings.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.EmployerAccounts.Api.Mappings
+{
+    using AutoMapper;
+
+    public class UserMappings : Profile
+    {
+        public UserMappings()
+        {
+            this.CreateMap<TeamMember, TeamMemberViewModel>();
+        }
+    }
+}
