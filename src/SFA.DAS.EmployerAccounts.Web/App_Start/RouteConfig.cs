1f8ab88d506326137db3456fb166759587635835
Steve O'Hara <github@soharadesign.com>
2019-02-15 19:04:01 +0000

    [AML-3330] Added missing Catch-all route to EmployerAccounts.Web RouteConfig.cs

--- src/SFA.DAS.EmployerAccounts.Web/App_Start/RouteConfig.cs
+++ src/SFA.DAS.EmployerAccounts.Web/App_Start/RouteConfig.cs
@@ -13,8 +13,12 @@ namespace SFA.DAS.EmployerAccounts.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
+            routes.MapRoute(
+                name: "CatchAll",
+                url: "{*path}",
+                defaults: new { controller = "Error", action = "NotFound" });
         }
     }
 }
