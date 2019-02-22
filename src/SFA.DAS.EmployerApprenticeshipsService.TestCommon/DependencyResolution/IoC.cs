816c080cadd21525588a29586a0e0069d939ea7e
Steve O'Hara <github@soharadesign.com>
2019-02-22 14:43:07 +0000

    [AML-3330] Removed some redundant dependencies, projects stripped and running (without tests)

--- src/SFA.DAS.EmployerApprenticeshipsService.TestCommon/DependencyResolution/IoC.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.TestCommon/DependencyResolution/IoC.cs
@@ -4,7 +4,6 @@ using SFA.DAS.Commitments.Api.Client.Interfaces;
 using SFA.DAS.EAS.Application.DependencyResolution;
 using SFA.DAS.EAS.Domain.Configuration;
 using SFA.DAS.EAS.Domain.Interfaces;
-using SFA.DAS.EAS.Domain.Models.Account;
 using SFA.DAS.EAS.Infrastructure.DependencyResolution;
 using SFA.DAS.Events.Api.Client;
 using SFA.DAS.Messaging.Interfaces;
