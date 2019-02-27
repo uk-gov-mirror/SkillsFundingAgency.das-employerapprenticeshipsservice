464eee94515d907c84872b2e307a4af722c83c46
Steve O'Hara <github@soharadesign.com>
2019-02-27 06:37:15 +0000

    [AML-3392] Updated Send Transfer Connection Invitation tests to SFA.DAS.Testing v3.0.22

--- src/SFA.DAS.EmployerAccounts/Commands/SendTransferConnectionInvitation/SendTransferConnectionInvitationCommandHandler.cs
+++ src/SFA.DAS.EmployerAccounts/Commands/SendTransferConnectionInvitation/SendTransferConnectionInvitationCommandHandler.cs
@@ -6,6 +6,8 @@ using SFA.DAS.Hashing;
 
 namespace SFA.DAS.EmployerAccounts.Commands.SendTransferConnectionInvitation
 {
+    using System;
+
     public class SendTransferConnectionInvitationCommandHandler : IAsyncRequestHandler<SendTransferConnectionInvitationCommand, long>
     {
         private readonly IEmployerAccountRepository _employerAccountRepository;
@@ -35,6 +37,7 @@ namespace SFA.DAS.EmployerAccounts.Commands.SendTransferConnectionInvitation
             var receiverAccount = await _employerAccountRepository.GetAccountById(receiverAccountId);
             var senderUser = await _userRepository.GetUserByRef(message.UserRef.Value);
             var senderAccountTransferAllowance = await _transferAllowanceService.GetTransferAllowance(message.AccountId.Value);
+
             var transferConnectionInvitation = senderAccount.SendTransferConnectionInvitation(receiverAccount, senderUser, senderAccountTransferAllowance.RemainingTransferAllowance ?? 0);
 
             await _transferConnectionInvitationRepository.Add(transferConnectionInvitation);
