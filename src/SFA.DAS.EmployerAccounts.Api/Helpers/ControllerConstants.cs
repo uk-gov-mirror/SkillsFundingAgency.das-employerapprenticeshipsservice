b37bede455d5c5a96253f85cdf20fe9d0c57423b
Steve O'Hara <github@soharadesign.com>
2019-02-28 20:30:36 +0000

    [AML-3330] Stripped out the additional API migrations temporarily. These will be picked up properly as part of phase 2.

--- src/SFA.DAS.EmployerAccounts.Api/Helpers/ControllerConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SFA.DAS.EmployerAccounts.Api.Helpers
-{
-    public class ControllerConstants
-    {
-        public const string AccountHashedIdRouteKeyName = "hashedAccountId";
-    }
-}
--- src/SFA.DAS.EmployerAccounts.Types/ResourceViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.EmployerAccounts.Types
-{
-    // TODO: relocate?
-    public class ResourceViewModel : IAccountResource
-    {
-        public string Id { get; set; }
-        public string Href { get; set; }
-    }
-}
