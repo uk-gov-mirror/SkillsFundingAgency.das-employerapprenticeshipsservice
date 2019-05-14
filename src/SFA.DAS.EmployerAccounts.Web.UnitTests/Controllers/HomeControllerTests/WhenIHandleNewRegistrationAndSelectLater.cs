3c143d6b105bc6ffaa65c85a456a028fbc1317a5
tom-gough <tom@tgsd.co.uk>
2019-05-14 09:11:46 +0100

    Improved test coverage

---
+++ src/SFA.DAS.EmployerAccounts.Web.UnitTests/Controllers/HomeControllerTests/WhenIHandleNewRegistrationAndSelectLater.cs
@@ -0,0 +1,86 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Authentication;
+using SFA.DAS.Authorization;
+using SFA.DAS.EmployerAccounts.Configuration;
+using SFA.DAS.EmployerAccounts.Interfaces;
+using SFA.DAS.EmployerAccounts.Web.Controllers;
+using SFA.DAS.EmployerAccounts.Web.Models;
+using SFA.DAS.EmployerAccounts.Web.Orchestrators;
+using SFA.DAS.EmployerAccounts.Web.ViewModels;
+
+namespace SFA.DAS.EmployerAccounts.Web.UnitTests.Controllers.HomeControllerTests
+{
+    public class WhenIHandleNewRegistrationAndSelectLater
+    {
+        private Mock<IAuthenticationService> _owinWrapper;
+        private HomeController _homeController;
+        private EmployerAccountsConfiguration _configuration;
+        private string ExpectedUserId = "123ABC";
+        private Mock<ICookieStorageService<ReturnUrlModel>> _returnUrlCookieStorage;
+        private string ExpectedReturnUrl = "test.com";
+
+        [SetUp]
+        public void Arrange()
+        {
+            _owinWrapper = new Mock<IAuthenticationService>();
+            _configuration = new EmployerAccountsConfiguration();
+            _returnUrlCookieStorage = new Mock<ICookieStorageService<ReturnUrlModel>>();
+
+            _homeController = new HomeController(
+                _owinWrapper.Object, Mock.Of<HomeOrchestrator>(), _configuration, Mock.Of<IAuthorizationService>(),
+                Mock.Of<IMultiVariantTestingService>(), Mock.Of<ICookieStorageService<FlashMessageViewModel>>(), _returnUrlCookieStorage.Object);
+        }
+
+
+        [Test]
+        public async Task ThenTheClaimsAreRefreshedForThatUserWhenAuthenticated()
+        {
+            //Arrange
+            _owinWrapper.Setup(x => x.GetClaimValue("sub")).Returns(ExpectedUserId);
+
+            //Act
+            await _homeController.HandleNewRegistration("later");
+
+            //Assert
+            _owinWrapper.Verify(x => x.UpdateClaims(), Times.Once);
+        }
+
+        [Test]
+        public async Task ThenThePageRedirectsToYouHaveRegisteredIfThereIsNoReturnUrlCookie()
+        {
+            //Arrange
+            //Act
+            var actual = await _homeController.HandleNewRegistration("later");
+
+            //Assert
+            Assert.IsNotNull(actual);
+            Assert.IsAssignableFrom<RedirectToRouteResult>(actual);
+            var actualRedirect = actual as RedirectToRouteResult;
+            Assert.IsNotNull(actualRedirect);
+            Assert.AreEqual("YouHaveRegistered", actualRedirect.RouteValues["action"]);
+        }
+
+        [Test]
+        public async Task ThenThePageRedirectsToReturnUrlCookie()
+        {
+            //Arrange
+            _returnUrlCookieStorage.Setup(x => x.Get("SFA.DAS.EmployerAccounts.Web.Controllers.ReturnUrlCookie"))
+                .Returns(new ReturnUrlModel() {Value = ExpectedReturnUrl });
+
+            //Act
+            var actual = await _homeController.HandleNewRegistration("later");
+
+            //Assert
+            Assert.IsNotNull(actual);
+            Assert.IsAssignableFrom<RedirectResult>(actual);
+            var actualRedirect = actual as RedirectResult;
+            Assert.IsNotNull(actualRedirect);
+            Assert.AreEqual(ExpectedReturnUrl, actualRedirect.Url);
+        }
+
+
+    }
+}
--- src/SFA.DAS.EmployerAccounts.Web.UnitTests/Controllers/HomeControllerTests/WhenINavigateToConfirmWhoYouAreWithAReturnUrl.cs
+++ src/SFA.DAS.EmployerAccounts.Web.UnitTests/Controllers/HomeControllerTests/WhenINavigateToConfirmWhoYouAreWithAReturnUrl.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Authentication;
 using SFA.DAS.Authorization;
-using SFA.DAS.EmployerAccounts.Authorization;
 using SFA.DAS.EmployerAccounts.Configuration;
 using SFA.DAS.EmployerAccounts.Interfaces;
 using SFA.DAS.EmployerAccounts.Web.Controllers;
@@ -13,22 +16,26 @@ using SFA.DAS.EmployerAccounts.Web.ViewModels;
 
 namespace SFA.DAS.EmployerAccounts.Web.UnitTests.Controllers.HomeControllerTests
 {
-    public class WhenIHandleNewRegistration
+    public class WhenINavigateToConfirmWhoYouAreWithAReturnUrl
     {
         private Mock<IAuthenticationService> _owinWrapper;
         private HomeController _homeController;
         private EmployerAccountsConfiguration _configuration;
         private string ExpectedUserId = "123ABC";
+        private Mock<ICookieStorageService<ReturnUrlModel>> _returnUrlCookieStorage;
+        private string ExpectedReturnUrl = "test.com";
+        private string ReturnUrlCookieName = "SFA.DAS.EmployerAccounts.Web.Controllers.ReturnUrlCookie";
 
         [SetUp]
         public void Arrange()
         {
             _owinWrapper = new Mock<IAuthenticationService>();
             _configuration = new EmployerAccountsConfiguration();
+            _returnUrlCookieStorage = new Mock<ICookieStorageService<ReturnUrlModel>>();
 
             _homeController = new HomeController(
                 _owinWrapper.Object, Mock.Of<HomeOrchestrator>(), _configuration, Mock.Of<IAuthorizationService>(),
-                Mock.Of<IMultiVariantTestingService>(), Mock.Of<ICookieStorageService<FlashMessageViewModel>>(), Mock.Of<ICookieStorageService<ReturnUrlModel>>());
+                Mock.Of<IMultiVariantTestingService>(), Mock.Of<ICookieStorageService<FlashMessageViewModel>>(), _returnUrlCookieStorage.Object);
         }
 
 
@@ -36,13 +43,11 @@ namespace SFA.DAS.EmployerAccounts.Web.UnitTests.Controllers.HomeControllerTests
         public async Task ThenTheClaimsAreRefreshedForThatUserWhenAuthenticated()
         {
             //Arrange
-            _owinWrapper.Setup(x => x.GetClaimValue("sub")).Returns(ExpectedUserId);
-
             //Act
-            await _homeController.HandleNewRegistration();
+            _homeController.ConfirmWhoYouAre(ExpectedReturnUrl);
 
             //Assert
-            _owinWrapper.Verify(x => x.UpdateClaims(), Times.Once);
+            _returnUrlCookieStorage.Verify(x => x.Create(It.Is<ReturnUrlModel>(y => y.Value == ExpectedReturnUrl), ReturnUrlCookieName, 1));
         }
     }
 }
