82264fc26d18e5b49c4d5fbe48b85f5b3f97f381
Ashley-Bass <ashley.bass@education.gov.uk>
2019-06-12 12:22:00 +0100

    Merge fix

--- src/SFA.DAS.EAS.Portal.UnitTests/Worker/EventHandlers/Paye/AddedPayeSchemeEventHandlerTests.cs
+++ src/SFA.DAS.EAS.Portal.UnitTests/Worker/EventHandlers/Paye/AddedPayeSchemeEventHandlerTests.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoFixture;
 using FluentAssertions;
+using Microsoft.Azure.Amqp.Framing;
+using Microsoft.Extensions.Logging;
 using Moq;
 using NServiceBus;
 using NUnit.Framework;
-using SFA.DAS.EAS.Portal.Application.Commands;
-using SFA.DAS.EAS.Portal.Application.Commands.Paye;
+using SFA.DAS.EAS.Portal.Application.Services;
+using SFA.DAS.EAS.Portal.Client.Database.Models;
+using SFA.DAS.EAS.Portal.Client.Types;
 using SFA.DAS.EAS.Portal.Worker.EventHandlers.PayeScheme;
 using SFA.DAS.EmployerAccounts.Messages.Events;
 
@@ -14,54 +21,114 @@ namespace SFA.DAS.EAS.Portal.UnitTests.Worker.EventHandlers.Paye
 {
     [Parallelizable]
     [TestFixture]
-    class AddedPayeSchemeEventHandlerTests
+    class WhenPayeSchemeAddedTests
     {
-        private AddedPayeSchemeEventHandler _sut;
-        private Mock<ICommandHandler<PayeSchemeAddedCommand>> _commandHandlerMock;
-        private IMessageHandlerContext _messageHandlerMockContext;
+        AddedPayeSchemeEventHandler _sut;
+        Mock<IAccountDocumentService> _accountServiceMock;
+        Mock<IMessageContextInitialisation> _messageMock;
+        private Mock<ILogger<AddedPayeSchemeEventHandler>> _loggerMock;
+        AccountDocument _accountDoc;
+        IFixture Fixture = new Fixture();
+        private Mock<IMessageHandlerContext> MessageHandlerContext;
 
         [SetUp]
         public void SetUp()
         {
-            _commandHandlerMock = new Mock<ICommandHandler<PayeSchemeAddedCommand>>();
-            _messageHandlerMockContext = Mock.Of<IMessageHandlerContext>();
-            _sut = new AddedPayeSchemeEventHandler(_commandHandlerMock.Object);
+            _accountServiceMock = new Mock<IAccountDocumentService>();
+            _loggerMock = new Mock<ILogger<AddedPayeSchemeEventHandler>>();
+            _messageMock = new Mock<IMessageContextInitialisation>();
+            _sut = new AddedPayeSchemeEventHandler(_accountServiceMock.Object,_messageMock.Object,_loggerMock.Object);
+
+            Fixture.Customize<Account>(a => a
+                .With(acc => acc.Id, 1));
+            _accountDoc = Fixture
+                .Build<AccountDocument>()
+                .Create();
+
+            _accountServiceMock.Setup(mock => mock.Get(1, It.IsAny<CancellationToken>())).ReturnsAsync(_accountDoc);
+
+            MessageHandlerContext = new Mock<IMessageHandlerContext>();
+            MessageHandlerContext.Setup(c => c.MessageId).Returns("ThisIsAMessage");
+
+            var messageHeaders = new Mock<IReadOnlyDictionary<string, string>>();
+            messageHeaders.SetupGet(c => c["NServiceBus.TimeSent"]).Returns(DateTime.Now.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss:ffffff Z", CultureInfo.InvariantCulture));
+            MessageHandlerContext.Setup(c => c.MessageHeaders).Returns(messageHeaders.Object);
         }
 
         [Test]
         [Category("UnitTest")]
-        public async Task ShouldAddPayeSchemeToAccount()
+        public async Task ShouldGetAccount()
         {
             // Arrange
             var userRef = Guid.NewGuid();
-            var payeEvent = new AddedPayeSchemeEvent
+            var payeCommand = new AddedPayeSchemeEvent{
+                AccountId = 1,
+                UserName = "Bob",
+                UserRef = userRef,
+                PayeRef = "payepayepaye" };
+
+
+            // Act
+            await _sut.Handle(payeCommand,MessageHandlerContext.Object);
+
+            // Assert
+            _accountServiceMock.VerifyAll();
+        }
+
+        [Test]
+        [TestCase(0, Description = "No existing PAYE schemes")]
+        [TestCase(2, Description = "Multiple existing PAYE, not same")]
+        [Category("UnitTest")]
+        public async Task ShouldAddPayeSchemeToAccountWhenNotExists(int numOfExistingPayeSchemes)
+        {
+            // Arrange
+            _accountDoc.Account.PayeSchemes = Fixture.CreateMany<PayeScheme>(numOfExistingPayeSchemes).ToList();
+            var userRef = Guid.NewGuid();
+            var payeCommand = new AddedPayeSchemeEvent
             {
                 AccountId = 1,
-                Created = new DateTime(2019, 5, 31),
-                PayeRef = "PayePayePaye",
                 UserName = "Bob",
-                UserRef = userRef
+                UserRef = userRef,
+                PayeRef = "payepayepaye"
             };
 
-            PayeSchemeAddedCommand resultCommand = null;
-
-            _commandHandlerMock
-                .Setup(mock => mock.Handle(It.IsAny<PayeSchemeAddedCommand>(), It.IsAny<CancellationToken>()))
-                .Callback((PayeSchemeAddedCommand command, CancellationToken ct) => 
+            AccountDocument resultAccountDoc = null;
+            _accountServiceMock.Setup(mock => mock.Save(It.Is<AccountDocument>(ad => ad.AccountId == 1), It.IsAny<CancellationToken>()))
+                .Callback((AccountDocument accountDoc, CancellationToken ct) =>
                 {
-                    resultCommand = command;
+                    resultAccountDoc = accountDoc;
                 })
                 .Returns(Task.CompletedTask);
 
-            // Act 
-            await _sut.Handle(payeEvent, _messageHandlerMockContext);
+            // Act
+            await _sut.Handle(payeCommand,MessageHandlerContext.Object);
 
             // Assert
-            resultCommand.AccountId.Should().Be(1);
-            resultCommand.Created.Should().Be(new DateTime(2019, 5, 31));
-            resultCommand.PayeRef.Should().Be("PayePayePaye");
-            resultCommand.UserName.Should().Be("Bob");
-            resultCommand.UserRef.Should().Be(userRef);
+            resultAccountDoc.Account.PayeSchemes.Count.Should().Be(numOfExistingPayeSchemes + 1);
+        }
+
+        [Test]
+        [Category("UnitTest")]
+        public async Task ShouldNotUpdateAccountIfAlreadyExists()
+        {
+            // Arrange
+            var existingPaye = new PayeScheme { PayeRef = "payepayepaye" };
+            _accountDoc.Account.PayeSchemes = Fixture.CreateMany<PayeScheme>(3).ToList();
+            _accountDoc.Account.PayeSchemes.Add(existingPaye);
+            var userRef = Guid.NewGuid();
+            var payeCommand = new AddedPayeSchemeEvent
+            {
+                AccountId = 1,
+                UserName = "Bob",
+                UserRef = userRef,
+                PayeRef = "payepayepaye"
+            };
+
+            // Act
+            await _sut.Handle(payeCommand,MessageHandlerContext.Object);
+
+            // Assert
+            _accountServiceMock.Verify(mock => mock.Save(It.IsAny<AccountDocument>(), It.IsAny<CancellationToken>()), Times.Never);
         }
     }
 }
--- src/SFA.DAS.EAS.Portal.Worker/EventHandlers/PayeScheme/AddedPayeSchemeEventHandler.cs
+++ src/SFA.DAS.EAS.Portal.Worker/EventHandlers/PayeScheme/AddedPayeSchemeEventHandler.cs
@@ -1,28 +1,35 @@
-﻿using System.Threading.Tasks;
-using NServiceBus;
-using SFA.DAS.EAS.Portal.Application.Commands;
-using SFA.DAS.EAS.Portal.Application.Commands.Paye;
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.EAS.Portal.Application.Services;
 using SFA.DAS.EmployerAccounts.Messages.Events;
 
 namespace SFA.DAS.EAS.Portal.Worker.EventHandlers.PayeScheme
 {
-    public class AddedPayeSchemeEventHandler : IHandleMessages<AddedPayeSchemeEvent>
+    public class AddedPayeSchemeEventHandler : EventHandler<AddedPayeSchemeEvent>
     {
-        private readonly ICommandHandler<PayeSchemeAddedCommand> _handler;
 
-        public AddedPayeSchemeEventHandler(ICommandHandler<PayeSchemeAddedCommand> handler)
+        public AddedPayeSchemeEventHandler(
+            IAccountDocumentService accountService,
+            IMessageContextInitialisation messageContextInitialisation,
+            ILogger<AddedPayeSchemeEventHandler> logger)
+                : base(accountService,messageContextInitialisation,logger)
         {
-            _handler = handler;
+
         }
 
-        public async Task Handle(AddedPayeSchemeEvent message, IMessageHandlerContext context)
+        protected override async Task Handle(AddedPayeSchemeEvent command, CancellationToken cancellationToken = default)
         {
-            await _handler.Handle(new PayeSchemeAddedCommand(
-                message.AccountId,
-                message.UserName, 
-                message.UserRef,
-                message.PayeRef,
-                message.Created));
+            var accountDoc = await GetOrCreateAccountDocument(command.AccountId, cancellationToken);
+
+            var existingPaye = accountDoc.Account.PayeSchemes.FirstOrDefault(paye => paye.PayeRef == command.PayeRef);
+
+            if (existingPaye == null)
+            {
+                accountDoc.Account.PayeSchemes.Add(new Client.Types.PayeScheme { PayeRef = command.PayeRef });
+                await AccountDocumentService.Save(accountDoc, cancellationToken);
+            }
         }
     }
 }
