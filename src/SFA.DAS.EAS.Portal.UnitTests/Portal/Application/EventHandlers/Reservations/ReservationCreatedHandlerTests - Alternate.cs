aacff0a4c7c918350686a1e90992910c310d5c59
Corey <corey.faulconbridge@digital.education.gov.uk>
2019-06-21 12:33:18 +0100

    Reservation created unit tests alternative

---
+++ src/SFA.DAS.EAS.Portal.UnitTests/Portal/Application/EventHandlers/Reservations/ReservationCreatedHandlerTests - Alternate.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EAS.Portal.Application.EventHandlers.Reservations;
+using SFA.DAS.EAS.Portal.UnitTests.Portal.Helpers;
+using SFA.DAS.Reservations.Messages;
+
+namespace SFA.DAS.EAS.Portal.UnitTests.Portal.Application.EventHandlers.Reservations
+{
+    [TestFixture, Parallelizable]
+    public class ReservationCreatedHandlerTestsAlternate
+    {
+        private IFixture Fixture = new Fixture();
+        private ReservationCreatedHandler _sut;
+        private ReservationCreatedEvent _testEvent;
+        private AccountDocServiceMockHelper _accountDocServiceMockHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _accountDocServiceMockHelper = new AccountDocServiceMockHelper();
+            _sut = new ReservationCreatedHandler(_accountDocServiceMockHelper.AccountDocumentServiceMock.Object);
+            _testEvent = Fixture.Create<ReservationCreatedEvent>();
+        }
+
+        [Test]
+        public async Task Handle_WhenAccountDoesNotExist_ThenAccountDocumentIsSavedWithNewReservation()
+        {
+            // Arrange
+            _accountDocServiceMockHelper.SetUpNoAccountDocument(_testEvent.AccountId);
+
+            // Act 
+            await _sut.Handle(_testEvent);
+
+            //Assert
+            _accountDocServiceMockHelper
+                .SavedAccountDocument
+                .Account
+                .Organisations
+                .SingleOrDefault(org => org.AccountLegalEntityId == _testEvent.AccountLegalEntityId).Reservations
+                .Should()
+                .ContainSingle(x => x.Id == _testEvent.Id);
+        }
+
+        [Test]
+        public async Task Handle_WhenAccountDoesNotContainOrganisation_ThenAccountDocumentIsSavedWithNewReservation()
+        {
+            // Arrange
+            _accountDocServiceMockHelper.SetUpEmptyAccountDoc(_testEvent.AccountId);
+
+            // Act 
+            await _sut.Handle(_testEvent);
+
+            //Assert
+            _accountDocServiceMockHelper
+                .SavedAccountDocument
+                .Account
+                .Organisations
+                .SingleOrDefault(org => org.AccountLegalEntityId == _testEvent.AccountLegalEntityId).Reservations
+                .Should()
+                .ContainSingle(x => x.Id == _testEvent.Id);
+        }
+
+        [Test]
+        public async Task Handle_WhenAccountDoesContainOrganisationButNotReservation_ThenAccountDocumentIsSavedWithNewReservation()
+        {
+            // Arrange
+            _accountDocServiceMockHelper.SetUpAccountWithOrg(_testEvent.AccountId, _testEvent.AccountLegalEntityId);
+
+            // Act 
+            await _sut.Handle(_testEvent);
+
+            //Assert
+            _accountDocServiceMockHelper
+                .SavedAccountDocument
+                .Account
+                .Organisations
+                .SingleOrDefault(org => org.AccountLegalEntityId == _testEvent.AccountLegalEntityId).Reservations
+                .Should()
+                .ContainSingle(x => x.Id == _testEvent.Id);
+        }
+
+        [Test]
+        public void Handle_WhenAccountDoesContainOrganisationAndReservation_ThenExceptionIsThrown()
+        {
+            // Arrange
+            _accountDocServiceMockHelper.SetUpAccountWithReservation(_testEvent.AccountId, _testEvent.AccountLegalEntityId, _testEvent.Id);
+
+            // Act
+            var exception = Assert.ThrowsAsync<Exception>(() => _sut.Handle(_testEvent));
+
+            //Assert
+            Assert.IsTrue(exception.Message.StartsWith("Received ReservationCreatedEvent with"));
+        }
+    }
+}
---
+++ src/SFA.DAS.EAS.Portal.UnitTests/Portal/Helpers/AccountDocServiceMockHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using Moq;
+using Newtonsoft.Json;
+using SFA.DAS.EAS.Portal.Application.Services;
+using SFA.DAS.EAS.Portal.Client.Database.Models;
+using SFA.DAS.EAS.Portal.Client.Types;
+
+namespace SFA.DAS.EAS.Portal.UnitTests.Portal.Helpers
+{
+    internal class AccountDocServiceMockHelper
+    {
+        private IFixture Fixture = new Fixture();
+        internal Mock<IAccountDocumentService> AccountDocumentServiceMock { get; }
+        internal AccountDocument SavedAccountDocument { get; private set; }
+
+        public AccountDocServiceMockHelper()
+        {
+            Fixture.Customize<AccountDocument>(cus => cus.OmitAutoProperties());
+            
+            AccountDocumentServiceMock = new Mock<IAccountDocumentService>();
+
+            AccountDocumentServiceMock
+                .Setup(mock => mock.Save(It.IsAny<AccountDocument>(), It.IsAny<CancellationToken>()))
+                .Callback((AccountDocument acc, CancellationToken cancellationToken) =>
+                {
+                    SavedAccountDocument = acc;
+                })
+                .Returns(Task.CompletedTask);
+        }
+
+        internal void SetUpNoAccountDocument(long accountId)
+        {
+            AccountDocumentServiceMock
+                .Setup(mock => mock.GetOrCreate(accountId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new AccountDocument(accountId));
+        }
+
+
+        internal void SetUpEmptyAccountDoc(long accountId)
+        {
+            var accountDocument = JsonConvert.DeserializeObject<AccountDocument>($"{{\"Account\": {{\"Id\": {accountId} }}}}");
+
+            AccountDocumentServiceMock
+                .Setup(mock => mock.GetOrCreate(accountId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(accountDocument);
+        }
+
+        internal void SetUpAccountWithOrg(long accountId, long accountLegalEntityId)
+        {
+            var accountDocument = Fixture
+                .Build<AccountDocument>()
+                .With(ad => ad.Account, Fixture.Build<Account>().With(acc => acc.Id, accountId).Create())
+                .Create();
+
+            var organisation = accountDocument.Account.Organisations.RandomElement();
+            organisation.AccountLegalEntityId = accountLegalEntityId;
+            organisation.Reservations = new List<Reservation>();
+
+            AccountDocumentServiceMock.Setup(s => s.GetOrCreate(accountId, It.IsAny<CancellationToken>())).ReturnsAsync(accountDocument);
+        }
+
+        internal void SetUpAccountWithReservation(long accountId, long accountLegalEntityId, Guid reservationId)
+        {
+            var accountDocument = Fixture
+                .Build<AccountDocument>()
+                .With(ad => ad.Account, Fixture.Build<Account>().With(acc => acc.Id, accountId).Create())
+                .Create();
+
+            var organisation = accountDocument.Account.Organisations.RandomElement();
+            organisation.AccountLegalEntityId = accountLegalEntityId;
+            var reservation = organisation.Reservations.RandomElement();
+            reservation.Id = reservationId;
+
+            AccountDocumentServiceMock.Setup(s => s.GetOrCreate(accountId, It.IsAny<CancellationToken>())).ReturnsAsync(accountDocument);
+        }
+    }
+}
