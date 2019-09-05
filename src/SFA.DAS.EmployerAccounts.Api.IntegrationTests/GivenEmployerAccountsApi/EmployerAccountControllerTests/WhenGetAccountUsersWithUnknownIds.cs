56d666c00df41eb83a5035166481c918e4dbc6c9
Neil Winstanley <Winsto@users.noreply.github.com>
2019-09-05 18:50:22 +0100

    AML-3882: move next test over.

---
+++ src/SFA.DAS.EmployerAccounts.Api.IntegrationTests/GivenEmployerAccountsApi/EmployerAccountControllerTests/WhenGetAccountUsersWithUnknownIds.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerAccounts.Api.IntegrationTests.Helpers;
+using SFA.DAS.EmployerAccounts.Api.Types;
+
+namespace SFA.DAS.EmployerAccounts.Api.IntegrationTests.GivenEmployerAccountsApi.EmployerAccountControllerTests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class WhenGetAccountUsersWithUnknownIds
+    :GivenEmployerAccountsApi
+    {
+        [Test]
+        public void ThenEmptyTeamMembersCollectionIsReturned()
+        {
+            Response
+                .ShouldHaveContentOfType<List<TeamMember>>();
+
+            Response.GetContent<List<TeamMember>>()
+                .Should()
+                .BeEmpty();
+        }
+
+        [Test]
+        public void ThenOkResponseIsReturn()
+        {
+            Response
+                .StatusCode
+                .Should()
+                .Be(HttpStatusCode.OK);
+        }
+        protected override string GetRequestUri()
+        {
+            return @"https://localhost:44330/api/accounts/MADE*UP*ID/users";
+        }
+    }
+}
