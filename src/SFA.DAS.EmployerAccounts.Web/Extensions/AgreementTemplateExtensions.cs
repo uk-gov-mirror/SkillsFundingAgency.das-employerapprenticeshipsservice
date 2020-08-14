6d5b4016770540e8af6671f2868f08ba9c745238
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-08-14 08:41:08 +0100

    expire-old-agreements

--- src/SFA.DAS.EmployerAccounts.Web/Extensions/AgreementTemplateExtensions.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Extensions/AgreementTemplateExtensions.cs
@@ -5,17 +5,10 @@ namespace SFA.DAS.EmployerAccounts.Web.Extensions
 {
     public static class AgreementTemplateExtensions
     {
-        public static string InsetText(this AgreementTemplateViewModel agreementTemplate, ICollection<OrganisationAgreementViewModel> organisationAgreementViewModel)
-        {
+        public const int AgreementVersionV3 = 3;
 
-            bool v3Accepted = false;
-            foreach (var organisation in organisationAgreementViewModel)
-            {
-                if (organisation.SignedDateText != string.Empty && organisation.Template.VersionNumber == 3)
-                {
-                    v3Accepted = true;
-                }
-            }
+        public static string InsetText(this AgreementTemplateViewModel agreementTemplate, ICollection<OrganisationAgreementViewModel> organisationAgreementViewModel)
+        {           
 
             switch (agreementTemplate.VersionNumber)
             {
@@ -26,10 +19,25 @@ namespace SFA.DAS.EmployerAccounts.Web.Extensions
                 case 3:
                     return "This is a new agreement.";
                 case 4:
+                    bool v3Accepted = GetV3AgreementStatus(organisationAgreementViewModel);
                     return v3Accepted ? "This is a variation to the agreement we published 9 January 2020. You only need to accept it if you want to access incentive payments for hiring a new apprentice." : "This is a new agreement.";
                 default:
                     return string.Empty;
             }
+        }
+
+        private static bool GetV3AgreementStatus(ICollection<OrganisationAgreementViewModel> organisationAgreementViewModel)
+        {
+            bool v3Accepted = false;
+            foreach (var organisation in organisationAgreementViewModel)
+            {
+                if (organisation.SignedDateText != string.Empty && organisation.Template.VersionNumber == AgreementVersionV3)
+                {
+                    v3Accepted = true;
+                }
+            }
+
+            return v3Accepted;
         }
     }
 }
